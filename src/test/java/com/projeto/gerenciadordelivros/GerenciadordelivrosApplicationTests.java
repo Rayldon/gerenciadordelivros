1f164ec3e0b554af36c58c35ea8a618ac3fbe22a
Rayldon <r4yldon@gmail.com>
2026-02-25 09:14:11 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/projeto/gerenciadordelivros/GerenciadordelivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciadordelivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordelivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
