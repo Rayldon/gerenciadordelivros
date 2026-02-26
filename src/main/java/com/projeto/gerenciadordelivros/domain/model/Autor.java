685592d75d158aed144ef06418977fb542b4f051
Rayldon <r4yldon@gmail.com>
2026-02-25 21:44:52 -0300

    Validar nome obrigatório de autor

--- src/main/java/com/projeto/gerenciadordelivros/domain/model/Autor.java
+++ src/main/java/com/projeto/gerenciadordelivros/domain/model/Autor.java
@@ -1,10 +1,15 @@
 package com.projeto.gerenciadordelivros.domain.model;
 
+import com.projeto.gerenciadordelivros.domain.exception.RegraNegocioException;
+
 public class Autor {
 
     private String nome;
 
     public Autor(String nome) {
+        if (nome == null || nome.trim().isEmpty()) {
+            throw new RegraNegocioException("Nome do autor e obrigatorio.");
+        }
         this.nome = nome;
     }
 
@@ -12,3 +17,4 @@ public class Autor {
         return nome;
     }
 }
+
